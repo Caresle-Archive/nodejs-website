c6b0d81cb84fb70d2f162198d71ed077b5d1abe7
Caresle <carabesle@gmail.com>
2021-07-26 21:26:51 -0600

    Basic structure

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const path = require('path')
+const indexRoutes = require('./routes/index.routes')
+const PORT = process.env.PORT || 3000
+
+// Express config
+app.set('views', path.join(__dirname, 'views'))
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+
+// routes
+app.use(indexRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server on PORT ${PORT}`)
+})
